0fd995d9c6844af1555ce78413ff91ba328f0451
RvDmitry <rvdmitry@gmail.com>
2020-05-17 22:44:18 +0600

    2. Каркас шахматной доски[#266513]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -8,33 +8,42 @@ import java.util.Optional;
 
 /**
  * //TODO add comments.
- *
+ * Class Logic
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
  */
 public class Logic {
+    /**
+     * Массив содержит набор фигур на доске.
+     */
     private final Figure[] figures = new Figure[32];
+    /**
+     * Индекс характеризует количество фигур на доске.
+     */
     private int index = 0;
 
+    /**
+     * Метод добавляет фигуру на доску.
+     * @param figure Фигура
+     */
     public void add(Figure figure) {
         this.figures[this.index++] = figure;
     }
 
+    /**
+     * Метод определяет может ли фигура сделать ход.
+     * @param source Начальное поле
+     * @param dest Конечно поле
+     * @return true, если ход возможен, иначе false
+     */
     public boolean move(Cell source, Cell dest) {
         boolean rst = false;
-        boolean movement = true;
         int index = this.findBy(source);
         if (index != -1) {
             try {
                 Cell[] steps = this.figures[index].way(source, dest);
-                for (Cell step : steps) {
-                    if (this.findBy(step) != -1) {
-                        movement = false;
-                        break;
-                    }
-                }
-                if (movement && steps.length > 0 && steps[steps.length - 1].equals(dest)) {
+                if (movement(steps) && steps.length > 0 && steps[steps.length - 1].equals(dest)) {
                     rst = true;
                     this.figures[index] = this.figures[index].copy(dest);
                 }
@@ -45,6 +54,25 @@ public class Logic {
         return rst;
     }
 
+    /**
+     * Метод определяет, есть ли на пути движения фигуры другие фигуры, которые перегрождают ей путь.
+     * @param steps Набор полей по которым возможно движение фигуры
+     * @return true, если путь свободен, иначе false
+     */
+    private boolean movement(Cell[] steps) {
+        boolean free = true;
+        for (Cell step : steps) {
+            if (this.findBy(step) != -1) {
+                free = false;
+                break;
+            }
+        }
+        return free;
+    }
+
+    /**
+     * Метод очищает доску от фигур.
+     */
     public void clean() {
         for (int position = 0; position != this.figures.length; position++) {
             this.figures[position] = null;
@@ -52,6 +80,12 @@ public class Logic {
         this.index = 0;
     }
 
+    /**
+     * Метод определяет находится ли на заданном поле фигура.
+     * Если находится возвращает ее индекс в массиве фигур. Иначе вовзращает -1.
+     * @param cell Поле в котором нужно проверить, есть ли там фигура
+     * @return Индекс фигуры в массиве
+     */
     private int findBy(Cell cell) {
         int rst = -1;
         for (int index = 0; index != this.figures.length; index++) {
@@ -63,6 +97,10 @@ public class Logic {
         return rst;
     }
 
+    /**
+     * Метод переопределяет вывод массива фигур в виде строки.
+     * @return Строка
+     */
     @Override
     public String toString() {
         return "Logic{" +
